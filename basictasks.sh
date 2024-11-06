e80d46c76fd926710ad43f155552081fcf3fd7d0
ruheena-parveen <ruheena1310@gmail.com>
2024-11-06 18:09:23 +0530

    basictasks.sh

---
+++ basictasks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Usage: Run Basic linux Tasks Bash Shell Script
+# Author: Ruheena Parveen
+# -------------------------------------------------
+echo
+echo This script will run a few basic tasks 
+echo
+pwd
+echo
+ls
+echo
+whoami
+echo
+date
+echo
+cal
+echo
+touch a b c
+echo End off script
